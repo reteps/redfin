7818959c05431c2d947f32f8962b63ac073a2c28
Peter Stenger <peter.a.stenger@gmail.com>
2021-01-10 15:20:50 -0500

    upload to pypi

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Peter Stenger',                   # Type in your name
   author_email = 'peter.promotions.stenger@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/reteps/python-redfin',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/reteps/python-redfin/archive/0.1.tar.gz',    # I explain this later on
   keywords = ['redfin', 'api', 'wrapper'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests'
